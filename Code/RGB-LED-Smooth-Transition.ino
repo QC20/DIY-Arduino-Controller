4c1b35241720ebb1aefd89823396a0df05d256d0
Jonas <36644388+QC20@users.noreply.github.com>
2023-09-05 20:10:06 +0200

    Update RGB-LED-Smooth-Transition.ino

--- Code/RGB-LED-Smooth-Transition.ino
+++ Code/RGB-LED-Smooth-Transition.ino
@@ -1,52 +1,63 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
-#define RED_PIN PB1
-#define GREEN_PIN PB2
-#define BLUE_PIN PB3
+#define PWM_DELAY_MS 10
 
-void PWM_init() {
-    // Set PB1 (OC1A), PB2 (OC1B), and PB3 (OC2) as output for PWM
-    DDRB |= (1 << RED_PIN) | (1 << GREEN_PIN) | (1 << BLUE_PIN);
+void initPWM() {
+    // Set PB1 (OC1A), PB2 (OC1B), and PB3 (OC2A) as output for PWM
+    DDRB |= (1 << PB1) | (1 << PB2) | (1 << PB3);
 
-    // Configure Timer1 and Timer2 for Fast PWM mode
-    TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11) | (1 << WGM10);
-    TCCR1B |= (1 << WGM12) | (1 << CS10); // No prescaler
+    // Set non-inverted fast PWM mode with 8-bit resolution for Timer 1 (PB1 and PB2)
+    TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 << WGM10);
+    TCCR1B |= (1 << CS10) | (1 << WGM12);
 
-    TCCR2 |= (1 << WGM20) | (1 << WGM21) | (1 << COM21) | (1 << CS20); // Fast PWM, no prescaler
+    // Set non-inverted fast PWM mode with 8-bit resolution for Timer 2 (PB3)
+    TCCR2A |= (1 << COM2A1) | (1 << WGM20);
+    TCCR2B |= (1 << CS20) | (1 << WGM22);
 
-    // Initialize PWM values to 0 (LEDs off)
+    // Initialize the PWM values (0% duty cycle)
     OCR1A = 0;
     OCR1B = 0;
-    OCR2 = 0;
+    OCR2A = 0;
 }
 
-void fadeLED(uint8_t *ledValue, uint8_t direction, uint8_t delay_ms) {
-    // Gradually increase or decrease LED brightness
-    for (uint8_t i = 0; i < 255; i++) {
-        *ledValue += direction;
-        _delay_ms(delay_ms);
+void pwm_delay(int ms) {
+    for (int i = 0; i < ms; i++) {
+        _delay_ms(1);
     }
 }
 
 int main() {
-    PWM_init();
+    // Initialize PWM
+    initPWM();
 
     while (1) {
-        // Fade in red
-        fadeLED(&OCR1A, 1, 10);
-        // Fade out red
-        fadeLED(&OCR1A, -1, 10);
+        // Smooth transition from off to red (PB1)
+        for (int i = 0; i <= 255; i++) {
+            OCR1A = i;
+            _delay_ms(PWM_DELAY_MS);
+        }
 
-        // Fade in green
-        fadeLED(&OCR1B, 1, 10);
-        // Fade out green
-        fadeLED(&OCR1B, -1, 10);
+        // Smooth transition from red to green (PB1 to PB2)
+        for (int i = 255; i >= 0; i--) {
+            OCR1A = i;
+            OCR1B = 255 - i;
+            _delay_ms(PWM_DELAY_MS);
+        }
 
-        // Fade in blue
-        fadeLED(&OCR2, 1, 10);
-        // Fade out blue
-        fadeLED(&OCR2, -1, 10);
+        // Smooth transition from green to blue (PB2 to PB3)
+        for (int i = 0; i <= 255; i++) {
+            OCR1B = i;
+            OCR2A = 255 - i;
+            _delay_ms(PWM_DELAY_MS);
+        }
+
+        // Smooth transition from blue to off (PB3)
+        for (int i = 255; i >= 0; i--) {
+            OCR2A = i;
+            _delay_ms(PWM_DELAY_MS);
+        }
     }
 
     return 0;
